29cef38157881d2c653f03b2662b9daee95624b9
Pieter Pas <pieter.pas@outlook.com>
2022-09-28 12:09:12 +0200

    Update install scripts

--- scripts/install-eigen.sh
+++ scripts/install-eigen.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/env bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 
-if [ -z "${VIRTUAL_ENV+x}" ]; then
-    echo "No active virtual environment, refusing to install."
-    exit 1
+prefix="$1" # Installation directory prefix
+build_type="${2:-RelWithDebInfo}"
+version="3.4.0"
+
+if [ -z "$prefix" ]; then
+    if [ -z "$VIRTUAL_ENV" ]; then
+        echo "No active virtual environment, refusing to install."
+        exit 1
+    else
+        prefix="$VIRTUAL_ENV"
+    fi
 fi
 
-build_type="${1:-RelWithDebInfo}"
-
 set -ex
-export CMAKE_PREFIX_PATH="$VIRTUAL_ENV:$CMAKE_PREFIX_PATH"
-export PKG_CONFIG_PATH="$VIRTUAL_ENV/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CMAKE_PREFIX_PATH="$prefix:$CMAKE_PREFIX_PATH"
+export PKG_CONFIG_PATH="$prefix/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 pushd /tmp
 
-# Eigen Linear Algebra
-rm -rf eigen
-git clone --single-branch --depth=1 --branch 3.4.0 \
+# Eigen
+[ -d eigen ] \
+ || git clone --single-branch --depth=1 --branch "$version" \
     https://gitlab.com/libeigen/eigen.git
 pushd eigen
-cmake -Bbuild -S. \
-    -D CMAKE_INSTALL_PREFIX="$VIRTUAL_ENV" \
-    -D CMAKE_BUILD_TYPE="${build_type}"
-cmake --build build -j
-cmake --install build
+cmake -S. -Bbuild \
+    -G "Ninja Multi-Config" \
+    -D CMAKE_INSTALL_PREFIX="$prefix"
+cmake --build build -j --config $build_type
+cmake --install build --config $build_type
 popd
 
 popd
--- scripts/install-gtest.sh
+++ scripts/install-gtest.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/env bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 
-if [ -z "${VIRTUAL_ENV+x}" ]; then
-    echo "No active virtual environment, refusing to install."
-    exit 1
+prefix="$1" # Installation directory prefix
+build_type="${2:-RelWithDebInfo}"
+version="main"
+
+if [ -z "$prefix" ]; then
+    if [ -z "$VIRTUAL_ENV" ]; then
+        echo "No active virtual environment, refusing to install."
+        exit 1
+    else
+        prefix="$VIRTUAL_ENV"
+    fi
 fi
 
-build_type="${1:-RelWithDebInfo}"
-
 set -ex
-export CMAKE_PREFIX_PATH="$VIRTUAL_ENV:$CMAKE_PREFIX_PATH"
-export PKG_CONFIG_PATH="$VIRTUAL_ENV/lib/pkgconfig:$PKG_CONFIG_PATH"
+export CMAKE_PREFIX_PATH="$prefix:$CMAKE_PREFIX_PATH"
+export PKG_CONFIG_PATH="$prefix/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 pushd /tmp
 
-# GoogleTest framework
-rm -rf googletest
-git clone --single-branch --depth=1 --branch main \
+# Google Test
+[ -d googletest ] \
+ || git clone --single-branch --depth=1 --branch "$version" \
     https://github.com/google/googletest.git
 pushd googletest
-cmake -Bbuild -S. \
-    -D CMAKE_INSTALL_PREFIX="$VIRTUAL_ENV" \
-    -D CMAKE_BUILD_TYPE="${build_type}"
-cmake --build build -j
-cmake --install build
+cmake -S. -Bbuild \
+    -G "Ninja Multi-Config" \
+    -D CMAKE_INSTALL_PREFIX="$prefix"
+cmake --build build -j --config $build_type
+cmake --install build --config $build_type
 popd
 
 popd
